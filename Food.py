2c6bc8f7039a677053a44a17d19603d717eea357
LuizFried <luifried50@gmail.com>
2023-07-29 20:54:46 -0300

    Inicio

---
+++ Food.py
@@ -0,0 +1,21 @@
+import customtkinter as ctk
+
+
+janela = ctk.CTk()
+janela.title('Wartale Foods')
+janela.geometry('500x400')
+janela.resizable(True,True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+janela.mainloop()
