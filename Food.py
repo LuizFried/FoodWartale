2eee6b19c2d38d440bb7d40d73ba14f18e0a9824
LuizFried <luifried50@gmail.com>
2023-07-30 02:28:04 -0300

    Update Food.py

--- Food.py
+++ Food.py
@@ -1,18 +1,25 @@
 import customtkinter as ctk
-import tkinter
+import tkinter as tk
 
-janela = ctk.CTk()
+
+janela = tk.Tk()
 janela.title('Wartale Foods')
 janela.geometry('500x400')
-janela.configure(background='white')
+janela.configure(background='#000000')
 janela.resizable(True,True)
 
 
-texto = ctk.CTkLabel(janela, text='Atributo:',bg_color='white', fg_color='cyan',corner_radius=1000)
+texto = ctk.CTkLabel(janela, text='Atributo:', bg_color='#000021', fg_color='green', corner_radius=100,
+                     font=('Monument Extended', 13, 'bold'))
 texto.grid(row=0, column=0, padx=10, pady=10)
-etexto = ctk.CTkEntry(janela,bg_color='black',fg_color='red',)
-etexto.grid(row=0, column=1, padx=1, pady=10)
+etexto = ctk.CTkEntry(janela, bg_color='black', fg_color='grey', font=('Monument Extended', 13, 'bold'),
+                      corner_radius=100,width=200)
+etexto.grid(row=0, column=1, padx=1, pady=10, sticky='nwse')
 
+button = ctk.CTkButton(janela,text='Buscar', fg_color='Gold', bg_color='#000000', corner_radius=100,
+                       text_color='black', width=70,
+                       font=('Monument Extended', 13, 'bold'))
+button.grid(row=0, column=4, padx=10, pady=10)
 
 
 
