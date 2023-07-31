b41e4b850ff2251c7a5b3229954f35dd2577e1fc
LuizFried <luifried50@gmail.com>
2023-07-31 17:33:05 -0300

    v2

--- Food.py
+++ Food.py
@@ -2,22 +2,26 @@ from tkinter import ttk
 from tkinter import *
 import customtkinter as ctk
 import tkinter as tk
+from tkinter import messagebox
 
 
 janela = tk.Tk()
 janela.title('Wartale Foods')
-janela.geometry('500x400')
-janela.configure(background='white')
-janela.resizable(True,True)
+janela.geometry('580x300')
+janela.configure(background='#000000')
+janela.resizable(False,False)
 janela.wm_iconbitmap('wartale.ico')
 
 
-dict = {'EXP': 'Beer: 2 Trigos e 2 Mardanos ou 2 Trigos e 2 Etheroot'
-               'Fish Roll: 2 Trigos, 1 alga e 1 peixe'
-            }
+lst = [('Beer', '2-Trigos', '2-Mardanos', '', ''),
+       ('Beer', '2-Trigos', '2-Etheroot', '', ''),
+       ('Fish Roll', '2-Trigos', '1-Alga', '1-Peixe', ''),
+       ('Thalon Wine', '2-Trigos', '2-Miragewort', '', ''),
+       ('Wyzen Mead', '2-Trigos', '2-Royaljelly', '', '')]
 
-total_rows = len(dict)
-total_columns = 3
+
+total_rows = len(lst)
+total_columns = len(lst[0])
 class Table:
 
     def __init__(self, janela):
@@ -25,43 +29,38 @@ class Table:
         # code for creating table
         for i in range(total_rows):
             for j in range(total_columns):
-                self.e = Entry(janela, width=20, fg='blue',
-                               font=('Arial', 16, 'bold'))
+                self.e = Entry(janela, width=15, fg='#091174',bg='#c4c8c5',
+                               font=('Arial', 10, 'bold'))
 
-                self.e.grid(row=i, column=j)
-                self.e.insert(END, dict[i][j])
+                self.e.grid(row=i+2, column=j, padx=2, pady=5)
+                self.e.insert(END, lst[i][j])
 
 def food(x=''):
     global dict
     atributo = x.upper()
-    count = 0
+
     try:
         if atributo == "EXP":
-            for i in dict.items():
-                resp.configure(text=f'{dict["EXP"]}\n')
+            resp = Table(janela)
 
-
-            for k in dict.items():
-                for i in k:
-                    print(f'{k}' ,end='')
-                    print(f'{i}')
     except:
-        resp.configure(text='Atributo não reconhecido, tente novamente!')
+        messagebox.showerror('ERRO','Atributo não reconhecido, tente novamente!')
 
 
 
-texto1 = ctk.CTkLabel(janela, text='Atributo:', corner_radius=100, bg_color='#000000')
-texto1.grid(row=0, column=0, padx=10, pady=10)
+texto1 = ctk.CTkLabel(janela, text='Atributo:', corner_radius=100,fg_color='#091174',text_color='white',
+                      bg_color='#000000')
+texto1.grid(row=0, column=0, padx=2, pady=10)
 
-etexto = ctk.CTkEntry(janela, width=40, corner_radius=100, bg_color='#000000')
-etexto.grid(row=0, column=1, padx=10, pady=10)
+etexto = ctk.CTkEntry(janela, width=100, corner_radius=100, bg_color='#000000')
+etexto.grid(row=0, column=1, padx=2, pady=10)
 
-button = ctk.CTkButton(janela, text='Buscar', hover_color='red',corner_radius=100, bg_color='#000000',
+button = ctk.CTkButton(janela,width=100, text='Buscar', hover_color='red',corner_radius=100, bg_color='#000000',
                        command=lambda: food(etexto.get()))
-button.grid(row=1, column=1, padx=10, pady=10)
+button.grid(row=0, column=2, padx=2, pady=10)
 
-resp = tk.Table(janela,  text='', width=200, bg_color='red', fg_color='cyan')
-resp.grid(row=2, column=1, padx=10, pady=10)
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-# Python program to create a table
-
 from tkinter import *
 
 
@@ -7,7 +5,6 @@ class Table:
 
     def __init__(self, root):
 
-        # code for creating table
         for i in range(total_rows):
             for j in range(total_columns):
                 self.e = Entry(root, width=20, fg='blue',
@@ -24,12 +21,11 @@ lst = [(1, 'Raj', 'Mumbai', 19),
        (4, 'Rachna', 'Mumbai', 21),
        (5, 'Shubham', 'Delhi', 21)]
 
-# find total number of rows and
-# columns in list
+
 total_rows = len(lst)
 total_columns = len(lst[0])
 
-# create root window
+
 root = Tk()
 t = Table(root)
 root.mainloop()
